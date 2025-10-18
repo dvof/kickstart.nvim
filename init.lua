c08d19f68d9add4bff04eaf1bf5fee27516b37d5
Daan v O <daan.vo@hotmail.com>
2025-10-18 11:21:42 +0200

    Add tab length and c/cpp formatter

--- init.lua
+++ init.lua
@@ -756,7 +756,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = {}
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
@@ -768,6 +768,8 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'clang-format' },
+        cpp = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -1012,7 +1014,13 @@ require('lazy').setup({
   },
 })
 
--- My keymaps
+-- [[ My setting options ]]
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+
+-- [[ My keymaps ]]
 -- Basics
 vim.keymap.set('i', 'jk', '<Esc>', { desc = 'Escape insert mode' })
 vim.keymap.set('c', 'jk', '<C-c>', { desc = 'Escape command' })
